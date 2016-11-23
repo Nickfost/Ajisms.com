f84c41b1744ae0130f64156f513f2698a3fce025
Dakota Creason <dakotacreason@gmail.com>
2016-11-23 12:02:49 -0600

    Stuff in place for social media

--- index.php
+++ index.php
@@ -4,7 +4,17 @@ starthtml();
 html("<script src='/js/index.js'></script>");
 html("<div class=\"alert\">");
 html("	<p>");
-html("		<strong id='quote'>".getquote()."</strong>");
+if(isset($_GET["specific"]) && $_GET["specific"] !== ""){
+	html("		<strong id='quote'>".allquotes($_GET["specific"])."</strong>");
+} elseif(isset($_GET["all"])){
+	$allquotes = "";
+	foreach (allquotes() as $quote) {
+		$allquotes .= $quote."<br/>";
+	}
+	html("		<strong id='quote'>".$allquotes."</strong>");
+} else {
+	html("		<strong id='quote'>".getquote()."</strong>");
+}
 html("	</p>");
 html("</div>");
 html("<br />");
