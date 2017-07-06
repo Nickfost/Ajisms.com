035a7a93f0732147cd7a0f25215c76dbc5e74329
Dakota Creason <dakotacreason@gmail.com>
2017-07-06 14:12:10 +0000

    Ajax works again

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
 require("include.php");
 starthtml();
-html("<script src='/js/index.js'></script>");
+html("<script>");
+html("function getquote(el){");
+html('	console.log("getting quote");');
+html("	var xhr = new XMLHttpRequest();");
+html("	xhr.open('GET', '/quote.php');");
+html("	xhr.onload = function() {");
+html("		if (xhr.status === 200) {");
+html('			document.querySelector("#quote").innerHTML = xhr.responseText;');
+html("		}");
+html("		else {");
+html("			console.log('Request failed.  Returned status of ' + xhr.status);");
+html("		}");
+html("	};");
+html("	xhr.send();");
+html("}");
+html("</script>"); //*/
+
 html("<div class=\"alert\">");
 html("	<p>");
 if(isset($_GET["specific"]) && $_GET["specific"] !== ""){
