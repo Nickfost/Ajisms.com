bb30e188f63634114a0b6d57d28f6fed5e9e29c4
AJWindy <AJWindmeyer@gmail.com>
2017-08-12 18:40:52 -0500

    Update include.php

--- include.php
+++ include.php
@@ -77,6 +77,32 @@ $quote = array(
 	"Do they have mexican hippies?",
 	"I miss the rape van",
 	"That's a nice tv, It'll make a great boat anchor",
+	"I farted.  The hole in my pants is now larger than it was originally",
+	"anyone have any decient low dinsity floppy diskettes for sale? preferably without mold growing on them",
+	"#windows2000forever",
+	"#poopin",
+	"free CRT monitors only $2.00",
+	"just wiped my ass with the toilet paper roll because my brother didn't change it out",
+	"Nothing quite like a ice cold honeydew to the armpit",
+	"What do you call a duck that can't swim? Dead.",
+	"So now I have a carburetor bowl of raisins. What the hell am I supposed to do with that?",
+	"hey @LonelyGoomba i shipped an Atari 2600 and a copy of ET your way. that aughta be shitty enough for you.",
+	"taco doritos and salsa isn't what i thought it would be. it still tastes like SHIT.",
+	"anyone know what to do if you shat green? It was weird and kinda made me scared"
+	"If I could poop fire that would really be a pain in the ass",
+	"Hey @Nickfost_ remember that blue cupcake? I ate 4 bags of skittles so am I gonna shart the rainbow?",
+	"yo @drpepper thanks for your support. my heartburn and assquakes were very warming.",
+	"admit it. we all have that crazy uncle who's farts sound like a fucking chainsaw.",
+	"ford can't post 1/4 mile strip videos on instagram any more. They limited video time to 15 seconds.",
+	"i have farted entirely too much today. Its like Auschwitz in my room right now.",
+	"75% of the time shoving gold bars up your ass so the government can't get them works 100% of the time.",
+	"well today has been shat on twice already, but i do have donuts",
+	"This product brought to you in part by Microsoft PowerPorn",
+	"well i just peed brown, will i poop yellow?",
+	"thanks to a previous tweet i may have burns on my ass now",
+	"why does a MS-DOS install floppy always break but a floppy with some 1990s bloatware work for eternity?",
+	"i just #farted and it smelled so bad the mold on this cheese beside me died.",
+	
 );
 function getquote(){
 	global $quote;
