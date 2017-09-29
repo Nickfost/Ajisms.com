ed7da7e5097168c6916d2529c7853377b7bab9ad
Nick Foster <me@nickfost.com>
2017-09-28 19:48:26 -0500

    Update include.php

--- include.php
+++ include.php
@@ -102,6 +102,7 @@ $quote = array(
 	"thanks to a previous tweet i may have burns on my ass now",
 	"why does a MS-DOS install floppy always break but a floppy with some 1990s bloatware work for eternity?",
 	"i just #farted and it smelled so bad the mold on this cheese beside me died.",
+	"I have made a noteworthy movement in the downstairs congress"
 	
 );
 function getquote(){
